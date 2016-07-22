eca8d68f27c5eb48819babb62fbc155a330404c6
LeonLiu <leon.liu@double2.com.twleon.liu@double2.com.tw>
2016-07-22 18:52:11 +0800

    create new project

---
+++ cocos/platform/android/java/.d
@@ -0,0 +1,4 @@
+/Users/leonliu/Documents/cocos2d-x-3.11.1/cocos/platform/android/java/gen/com/enhance/gameservice/IGameTuningService.java : \
+  /Users/leonliu/Documents/cocos2d-x-3.11.1/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl
+
+/Users/leonliu/Documents/cocos2d-x-3.11.1/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl :
